7dfb32d009cedadc051f0d4f75cf4673f6462f98
leeforster28 <leeforster@techniche.com.au>
2023-02-28 10:20:05 +1100

    Manifest Issues

--- content.js
+++ content.js
@@ -1,14 +1,23 @@
-document.getElementById('calculate').addEventListener('click', () => {
-  const data = document.getElementById('input').value;
-  fetchData(data);
-});
-
-document.getElementById('input').addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
+window.onload = () => {
+  document.getElementById('calculate').addEventListener('click', () => {
     const data = document.getElementById('input').value;
     fetchData(data);
-  }
-});
+  });
+
+  document.querySelectorAll('a').forEach((a) => {
+    a.addEventListener('click', (event) => {
+      event.preventDefault();
+      window.open(a.href, '_blank');
+    });
+  });
+
+  document.getElementById('input').addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      const data = document.getElementById('input').value;
+      fetchData(data);
+    }
+  });
+};
 
 async function fetchData(data) {
   // Send a message to the background script
